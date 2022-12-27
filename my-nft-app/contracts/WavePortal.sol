afa5d57c44cb533fba214c7ebee37ee2e9901234
Rum2001 <61676156+Rum2001@users.noreply.github.com>
2022-12-27 16:33:28 +0700

    commit

--- my-nft-app/contracts/WavePortal.sol
+++ my-nft-app/contracts/WavePortal.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
-        console.log("This my smart contracts");
+        console.log("This my smart contracts test waves");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
